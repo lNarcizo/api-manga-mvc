98a9308841fede06b0700b12952d050d1e79cc9f
Lucas Narcizo Neto <lucasnarcizo99@hotmail.com>
2022-05-31 18:02:52 -0300

    adicionando novas rotas models e controllers

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,20 @@ const { Router } = require('express');
 const LoginController = require('./app/Controller/LoginController');
 const AuthMidleWare = require("./app/Midlewares/AuthMidleware");
 const UserController = require("./app/Controller/UserController");
+const GeneroController = require("./app/Controller/GeneroController");
 
 const routes = new Router();
 
+// Rota de autenticação
 routes.post("/login", LoginController.index);
 
-routes.post("/usuario", UserController.save);
-routes.get("/usuario", AuthMidleWare, UserController.list);
+// Rotas de usuario
+routes.get("/lista-usuarios", AuthMidleWare, UserController.list);
+routes.post("/usuario/cadastro", UserController.save);
+
+// Rotas de genero
+routes.get("/genero/:name", AuthMidleWare, GeneroController.listOne);
+routes.get("/lista-generos", AuthMidleWare, GeneroController.list);
+routes.post("/genero/cadastro", AuthMidleWare, GeneroController.save);
 
 module.exports = routes;
